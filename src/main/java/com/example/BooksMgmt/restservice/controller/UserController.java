2c8b02c27af261c4ae848f77e6757b1662400e68
VishalVyavahare <vishal.vyavahare@aeratechnology.com>
2020-10-03 22:01:09 +0530

    Added lombok support and swagger implementation.

--- src/main/java/com/example/BooksMgmt/restservice/controller/UserController.java
+++ src/main/java/com/example/BooksMgmt/restservice/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.example.BooksMgmt.restservice.controller;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Api(tags = "Users API", value = "All the User related API")
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/v1/books")
 public class UserController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return name;
     }
